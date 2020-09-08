07594e431547f18d941b0db1157e6c159ddf4971
Jeremy Legendre <jslegendre@gmail.com>
2020-09-08 11:31:05 -0400

    Avoid crash if class is not found

--- JSRollCall/JSRollCall.m
+++ JSRollCall/JSRollCall.m
@@ -143,6 +143,7 @@ void malloc_zone_enumerator(task_t task, void *context, unsigned type, vm_range_
 }
 
 - (void)allObjectsOfClass:(Class)cls includeSubclass:(BOOL)shouldIncludeSubclasses performBlock:(void (^)(id))block {
+    if (!cls) return;
     dispatch_async(dispatch_get_main_queue(), ^{
         NSSet *results = [self allObjectsOfClass:cls includeSubclass:shouldIncludeSubclasses];
         [results enumerateObjectsUsingBlock:^(id obj, BOOL *stop){
